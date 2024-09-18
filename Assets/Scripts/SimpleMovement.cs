c0ca1a91c9cf5d90f7dfb6a70272ce5a4359c21f
Teffer31 <t.barmak7@gmail.com>
2024-09-18 04:13:47 +0200

    gravity

--- Assets/Scripts/SimpleMovement.cs
+++ Assets/Scripts/SimpleMovement.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 public class SimpleMovement : MonoBehaviour
 {
     // Variabele voor snelheid
-    public Vector3 velocity;   // De snelheid van het object (meter per seconde)
+    public Vector3 velocity;   // De snelheid van het object
+
+    public float gravity = -9.81f;
     
     // Update wordt één keer per frame aangeroepen
     void Update()
     {
         // Delta tijd om frame-afhankelijkheid te voorkomen
         float deltaTime = Time.deltaTime;
+
+        velocity.y += gravity * deltaTime;
         
         // Verminder de snelheid door wrijving of een andere kracht
         velocity *= 0.99f;  // Snelheid neemt met 1% af per frame als voorbeeld.
