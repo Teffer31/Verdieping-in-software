dc2b72e67f19b51dd8e143c7c07fbd2e42af5922
Teffer31 <t.barmak7@gmail.com>
2024-09-30 05:16:00 +0200

    updated detect collisions

--- Assets/Scripts/AABBCollision.cs
+++ Assets/Scripts/AABBCollision.cs
@@ -2,32 +2,50 @@ using UnityEngine;
 
 public class AABBCollision : MonoBehaviour
 {
-    // Referenties naar de twee objecten waar je de AABB collision voor wilt detecteren
     public Transform object1;
     public Transform object2;
 
-    // Grootte van de colliders (de half-extents van de bounding boxes)
-    public Vector3 size1;
-    public Vector3 size2;
+    public BoxCollider collider1;
+    public BoxCollider collider2;
+
+    private Vector3 halfExtents1;
+    private Vector3 halfExtents2;
+
+    private bool wasColliding = false;
+
+    void Start()
+    {
+        halfExtents1 = collider1.bounds.extents;
+        halfExtents2 = collider2.bounds.extents;
+    }
 
     void Update()
     {
-        if (IsColliding(object1.position, size1, object2.position, size2))
+        // Voer eerst een snelle afstandscheck uit
+        float distance = Vector3.Distance(object1.position, object2.position);
+        float combinedExtents = (halfExtents1 + halfExtents2).magnitude;
+
+        if (distance <= combinedExtents)
         {
-            Debug.Log("Collision Detected");
-        }
-        else
-        {
-            Debug.Log("No Collision");
+            bool isColliding = IsColliding(object1.position, halfExtents1, object2.position, halfExtents2);
+
+            if (isColliding && !wasColliding)
+            {
+                Debug.Log("Collision Detected");
+            }
+            else if (!isColliding && wasColliding)
+            {
+                Debug.Log("No Collision");
+            }
+
+            wasColliding = isColliding;
         }
     }
 
-    // Functie om AABB collisions te detecteren
-    bool IsColliding(Vector3 pos1, Vector3 size1, Vector3 pos2, Vector3 size2)
+    bool IsColliding(Vector3 pos1, Vector3 halfExtents1, Vector3 pos2, Vector3 halfExtents2)
     {
-        // Controleer of de objecten elkaar overlappen op alle assen
-        return (pos1.x - size1.x < pos2.x + size2.x && pos1.x + size1.x > pos2.x - size2.x) &&
-               (pos1.y - size1.y < pos2.y + size2.y && pos1.y + size1.y > pos2.y - size2.y) &&
-               (pos1.z - size1.z < pos2.z + size2.z && pos1.z + size1.z > pos2.z - size2.z);
+        return (pos1.x - halfExtents1.x < pos2.x + halfExtents2.x && pos1.x + halfExtents1.x > pos2.x - halfExtents2.x) &&
+               (pos1.y - halfExtents1.y < pos2.y + halfExtents2.y && pos1.y + halfExtents1.y > pos2.y - halfExtents2.y) &&
+               (pos1.z - halfExtents1.z < pos2.z + halfExtents2.z && pos1.z + halfExtents1.z > pos2.z - halfExtents2.z);
     }
 }
